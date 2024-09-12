fa1ce8167daf4b479588bcdfb0123acdaa12055b
Foul-Tarnished <107711110+Foul-Tarnished@users.noreply.github.com>
2024-09-12 16:24:52 +0200

    Change skipped shaders (thanks DMugetsu)
    Most skipped shaders are not needed now, and DMugetsu found shaders skips that workaround the black decals on character, and crash on dying in chalices etc

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -188,8 +188,8 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 }
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static constexpr std::array<u64, 5> skip_hashes = {0xc0cbc309, 0x77d1c63, 0xff7a6d7c,
-                                                       0xddfbac23, 0x896e7242};
+    static constexpr std::array<u64, 4> skip_hashes = {0xaa9d023d, 0x17a64a21, 0x94ec4dfb,
+                                                       0xc8854a11};
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
